328edf821ba0e427398288371487d9e500c3f42f
Abhishek <you@example.com>
2024-04-09 15:56:30 +0530

    first commit

---
+++ Javascript/open.js
@@ -0,0 +1,11 @@
+function open1() {
+  
+    var sidebar = document.querySelector('.sidebar');
+    if (sidebar.style.width === '300px') {
+        sidebar.style.width = '0';
+    } else {
+        sidebar.style.width = '300px';
+    }
+    
+
+}
